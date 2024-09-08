d84a2c6422115eaec26313c9e611cd5e46569e29
Chan Troi Moi <tonglanhchua@gmail.com>
2024-09-08 08:23:19 +0000

    update AudioContainer.jsx

--- src/components/AudioContainer.jsx
+++ src/components/AudioContainer.jsx
@@ -47,7 +47,7 @@ export default function AudioContainer({ audioList }) {
           )}
       </List>
       <div className="fixed bottom-1 left-1 right-1">
-        <p className="justify-center flex text-center text-violet-500 text-3xl pointer-events-none font-extrabold">
+        <p className="justify-center flex text-center text-amber-600 text-3xl pointer-events-none">
           {currentAudio?.title} | {currentAudio?.artist}
         </p>
         <audio className="w-full" ref={audioRef} controls />
