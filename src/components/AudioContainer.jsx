52de72c63a3839c9191618dee16bd2f12661a72e
Chan Troi Moi <tonglanhchua@gmail.com>
2024-09-08 07:17:08 +0000

    delete 1 file and update 3 files

--- src/components/AudioContainer.jsx
+++ src/components/AudioContainer.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import ItemAudio from "./ItemAudio";
+import logoUrl from "../assets/react.svg";
 
 export default function AudioContainer({ audioList }) {
   const [currentAudio, setCurrentAudio] = useState(null);
@@ -14,30 +15,29 @@ export default function AudioContainer({ audioList }) {
 
     if (currentAudio) {
       audioRef.current.src = currentAudio.url;
-    }
-    if (faviconLink) {
       document.title = currentAudio.title;
       faviconLink.href = logoUrl;
     }
   }, [currentAudio]);
   return (
     <>
-      <ul className="divide-y divide-slate-300 bg-slate-300 fixed top-8 bottom-20 left-1 right-1 overflow-y-auto">
-        {Array.isArray(audioList) &&
-          audioList.map(
-            (audio) =>
-              typeof audio === "object" && (
-                <ItemAudio
-                  key={audio.title}
-                  audio={audio}
-                  currentAudio={currentAudio}
-                  setCurrentAudio={setCurrentAudio}
-                />
-              )
-          )}
-      </ul>
       <div className="fixed bottom-1 left-1 right-1">
-        <audio ref={audioRef} controls />
+        <ul className="divide-y divide-slate-300 bg-slate-300 fixed top-20 bottom-15 overflow-y-auto">
+          {Array.isArray(audioList) &&
+            audioList.map(
+              (audio) =>
+                typeof audio === "object" &&
+                audio.title && (
+                  <ItemAudio
+                    key={audio.title}
+                    audio={audio}
+                    currentAudio={currentAudio}
+                    setCurrentAudio={setCurrentAudio}
+                  />
+                )
+            )}
+        </ul>
+        <audio className="w-full" ref={audioRef} controls />
       </div>
     </>
   );
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,6 @@
 export default function Nav({ children }) {
   return (
-    <nav className="py-4 px-6 text-sm font-medium justify-center flex fixed top-1 bg-slate-500 w-full z-50">
+    <nav className="py-4 px-6 text-sm font-medium justify-center flex fixed top-1 bg-transparent w-full z-50">
       <ul className="flex space-x-3">{children}</ul>
     </nav>
   );
